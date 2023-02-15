a87dd0dbccc310f16a85f9676330aa5d27e4c11d
fikretcure <fikretcure@gmail.com>
2023-02-15 07:30:35 +0300

    Added route in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name("users.")->prefix("users")->controller(UserController::class)->group(function () {
+    Route::name("index")->get(null, "index");
+    Route::name("store")->post(null, "store");
+    Route::name("show")->get("{id}", "show");
+    Route::name("update")->put("{id}", "update");
+    Route::name("destroy")->delete("{id}", "destroy");
 });
