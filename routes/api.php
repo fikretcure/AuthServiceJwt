af01b7fb9440ecb1fcf837c74c9239dafbfa66f1
fikretcure <fikretcure@gmail.com>
2023-02-15 19:51:45 +0300

    Updated api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,15 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::name("users.")->prefix("users")->controller(UserController::class)->group(function () {
-    Route::name("index")->get(null, "index");
-    Route::name("store")->post(null, "store");
-    Route::name("show")->get("{id}", "show");
-    Route::name("update")->put("{id}", "update");
-    Route::name("destroy")->delete("{id}", "destroy");
-});
+Route::middleware(AuthMiddleware::class)->group(function () {
+    Route::name("users.")->prefix("users")->controller(UserController::class)->group(function () {
+        Route::name("index")->get(null, "index");
+        Route::name("store")->post(null, "store")->withoutMiddleware(AuthMiddleware::class);
+        Route::name("show")->get("{id}", "show");
+        Route::name("update")->put("{id}", "update");
+        Route::name("destroy")->delete("{id}", "destroy");
+    });
 
-Route::name("authentication.")->prefix("authentication")->controller(AuthenticationController::class)->group(function () {
-    Route::name("login")->post("login", "login");
-    Route::name("show")->get("show", "show");
+    Route::name("authentication.")->prefix("authentication")->controller(AuthenticationController::class)->group(function () {
+        Route::name("login")->post("login", "login")->withoutMiddleware(AuthMiddleware::class);
+        Route::name("show")->get("show", "show");
+    });
 });
