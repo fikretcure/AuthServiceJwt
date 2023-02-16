e204ed877580d6769c5988b3d75ec72af2377835
fikretcure <fikretcure@gmail.com>
2023-02-16 16:39:41 +0300

    Added route in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\EndPointController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
@@ -29,4 +30,6 @@ Route::middleware(AuthMiddleware::class)->group(function () {
         Route::name("login")->post("login", "login")->withoutMiddleware(AuthMiddleware::class);
         Route::name("show")->get("show", "show");
     });
+
+    Route::name("endPoints.index")->get("end-points", EndPointController::class);
 });
